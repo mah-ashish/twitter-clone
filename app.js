8f8b11f7b0bb317cc8668a2cbf34f5cc4bb1db88
ashish maheshwari <taken705@gmail.com>
2019-03-30 11:48:44 +0530

    added api doc

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/chat', [checkLogin], chatRouter);
 
 //Home
 app.get('/', (req, res) => {
-    res.send('Home Page');
+    res.send('https://bit.ly/2Ui1Lgk');
 });
 
 const mongoUrl = process.env.mongoUrl;
